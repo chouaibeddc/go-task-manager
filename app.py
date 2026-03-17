d3df6b6d99ed9be5fb7c48a3af8e5f8b5682f85e
EL KHYATI Bouchaib <chouaib.elkhyati2000@gmail.com>
2026-03-17 07:30:08 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,93 @@
+# Imports
+from asyncio import current_task
+
+from flask import Flask , render_template , request , redirect
+from flask_assets import Environment , Bundle
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import sass
+
+
+# APP
+app = Flask(__name__)
+
+# Compiling SCSS To CSS
+with open("static/scss/styles.scss" , "r") as file_in:
+    with open("static/css/styles.css" , "w") as file_out:
+        css = sass.compile(string=file_in.read() , output_style='compressed')
+        file_out.write(css)
+        file_out.close()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Data Class
+class MyTask(db.Model):
+    id = db.Column(db.Integer ,primary_key=True )
+    content = db.Column(db.String(100) , nullable=False)
+    complete = db.Column(db.Integer , default=0)
+    created = db.Column(db.DateTime  , default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Task {self.id}"
+
+with app.app_context():
+    db.create_all()
+#Routes to webpages
+# Home page
+@app.route("/" , methods=['GET', 'POST'])
+def index():
+    # Ass a Task
+    if request.method == 'POST':
+        current_task = request.form['content']
+        if not current_task:
+            return redirect('/')
+        new_task = MyTask(content=current_task)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except Exception as e:
+            print(f'ERROR: {e}')
+            return f'ERROR: {e}'
+
+    # See all current tasks
+    else:
+        tasks = MyTask.query.order_by(MyTask.created).all()
+
+        return render_template("index.html" , tasks=tasks)
+
+
+# Delete an Item
+@app.route("/delete/<int:id>")
+def delete(id:int):
+    delete_task = MyTask.query.get_or_404(id)
+    try:
+        db.session.delete(delete_task)
+        db.session.commit()
+        return redirect('/')
+    except Exception as e:
+        print(f'ERROR: {e}')
+        return f'ERROR: {e}'
+
+# Edit an item
+@app.route("/edit/<int:id>" , methods=['GET', 'POST'])
+def edit(id:int):
+    task = MyTask.query.get_or_404(id)
+    if request.method == 'POST':
+        current_task = request.form['content']
+        if not current_task:
+            return redirect('/')
+        task.content = current_task
+        try:
+            db.session.commit()
+            return redirect('/')
+        except Exception as e:
+            print(f'ERROR: {e}')
+            return f'ERROR: {e}'
+    else:
+        return render_template('edit.html' , task=task)
+
+if __name__ == "__main__":
+    app.run(debug=True)
